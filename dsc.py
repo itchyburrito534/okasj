c24033de7b1894be3695a5e9b6a1a1d49022012b
itchyburrito534 <73443627+itchyburrito534@users.noreply.github.com>
2023-09-09 16:30:42 -0400

    Add files via upload

---
+++ dsc.py
@@ -0,0 +1,45 @@
+import time
+import requests
+from flask import Flask
+from threading import Thread
+import os
+ 
+token = os.getenv("TOKEN")
+channel_id = os.getenv("CHANNEL_ID")
+ 
+count = 0
+url = f"https://discord.com/api/v9/channels/{channel_id}/typing"
+headers = {
+  'authorization': token
+}
+ 
+ 
+app = Flask('')
+ 
+@app.route('/')
+def home():
+	return ("Bot is up")
+ 
+def run():
+  app.run(
+		host='0.0.0.0',
+		port=8080
+	)
+ 
+def keep_alive():
+	'''
+	Creates and starts new thread that runs the function run.
+	'''
+	t = Thread(target=run)
+	t.start()
+ 
+keep_alive()
+while True:
+	response = requests.request("POST", url, headers=headers)
+	if response.status_code != 204:
+		print(response.text)
+		print("Failed")
+	else:
+		count += 1
+		print(f"Sent - {count}")
+	time.sleep(10)
